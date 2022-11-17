2efadf56a8e3fd66143fbed1d1e0b8f686615815
Ricardo <ricardo.detoni@gmail.com>
2022-11-17 21:41:24 +0100

    Adds reading logic for payslip

--- src/main/java/com/example/payslip/bl/PayslipReader.java
+++ src/main/java/com/example/payslip/bl/PayslipReader.java
@@ -1,11 +1,17 @@
 package com.example.payslip.bl;
 
+import com.example.payslip.model.entities.Payslip;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.Month;
+import java.time.Year;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Component
 public class PayslipReader {
@@ -36,6 +42,30 @@ public PDDocument getFileDecrypted() throws IOException {
     }
 
     return document;
+}
+
+public Payslip getPayslipFromFile() throws IOException{
+    Payslip payslip = new Payslip();
+    PDDocument document = getFileDecrypted();
+    PDFTextStripper pdfTextStripper = new PDFTextStripper();
+    String payslipContent = pdfTextStripper.getText(document);
+
+    //gets month and year
+    String date = extractString("([0-9]{2}).([0-9]{2}).([0-9]{4})", payslipContent);
+    if(date.isEmpty()){
+        return null;
+    }else{
+        payslip.setMonth(Month.of(Integer.valueOf(date.substring(2, 4)) + 1));
+        payslip.setYear(Year.of(Integer.valueOf(date.substring(5, 9))));
+    }
+
+    return payslip;
+}
+
+private String extractString(String regex, String text){
+    Pattern pattern = Pattern.compile(regex);
+    Matcher matcher = pattern.matcher(text);
+    return matcher.find() ? matcher.group(1) : "";
 }
 
 }
